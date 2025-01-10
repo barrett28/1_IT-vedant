cd605f789c921eafad2dd5963ffd9cfc2bb13ba7
BharatSingh <bharat2808singh@gmail.com>
2025-01-10 16:21:19 +0530

    while loop

---
+++ 5_python/day3/while.py
@@ -0,0 +1,89 @@
+# n = int(input("enter anumber for printing table: "))
+# i = 1
+
+# while (i<=10):
+#     print(n,"*",i, "=", n*i)
+#     i = i+1
+
+# n = int(input("enter a number: "))
+# i= 1
+# while (i<=n):
+#     j=1
+#     while (j<=10):
+#         print(j*i)
+#         j =j+1
+#     i=i+1
+#     print()
+
+
+# ==============table print==================
+
+# n = int(input("enter the number to print a table: "))
+# i = 1
+# while (i<=10):
+#     print(n, "*",i, "=",n*i)
+#     i = i+1
+
+# ======================= enter number then 1->vaha tk k table===========
+# n = int(input("enter a number:"))
+# i = 1
+# while(i<=n):
+#     j = 1
+#     while(j<=10):
+#         print(i, "*", j, "=",i*j)
+#         j = j+1
+#     i = i+1
+#     print()
+
+
+# =========factorial=======================
+
+# n = int(input("enter a number: "))
+# fact=1
+# while(n>=1):
+#     fact = fact*n
+#     n = n-1
+# print(fact)
+
+# =================armstrong for===============
+
+# n = 153
+# arm = 0
+# for i in str(n):
+#     arm = arm+int(i)**3
+# print(arm)
+
+# if arm==n:
+#     print(n," it is an armstrong number")
+# else:
+#     print("no its not an armstrong number")
+    
+# ==================================armstrong while======
+
+# n = int(input("enter a number to check armstrong: "))
+# arm = 0
+# while (n>0):
+#     last = n%10
+#     arm = arm + last**3
+#     # print(arm)
+#     n = n//10
+# print(arm)
+
+# if arm==n:
+#     print("it is an armstrong")
+# else:
+#     print("its not an armstrong")
+    
+    
+  
+# =====================sum of digits================== 
+  
+# n = int(input("enter a number:"))
+# sum = 0
+# while(n>0):
+#     digit = n%10
+#     sum += digit
+#     n = n//10
+# print("sum of the digit is: ",sum)
+
+    
