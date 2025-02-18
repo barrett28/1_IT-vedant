862eb243fa9fae618c55d96402c37d0d37ace690
BharatSingh <bharat2808singh@gmail.com>
2025-02-18 19:00:00 +0530

    orm

---
+++ 6_django/day6/ORM/core/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import MarvelModel
+
+# Create your views here.
+
+def index(request):
+    mf = MarvelModel.objects.all()
+    return render(request, 'core/index.html', {'mf':mf})
