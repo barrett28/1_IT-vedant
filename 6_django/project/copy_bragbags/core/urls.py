c340d714bfa1c886e9f03d1940416e809cd13c3e
BharatSingh <bharat2808singh@gmail.com>
2025-03-05 16:21:28 +0530

    product page and cud day13

--- 6_django/project/copy_bragbags/core/urls.py
+++ 6_django/project/copy_bragbags/core/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('product/', views.product),
+    path('', views.index)
 ]
--- 6_django/project/copy_bragbags/core/views.py
+++ 6_django/project/copy_bragbags/core/views.py
@@ -4,6 +4,3 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'core/index.html')
-
-def product(request):
-    return render(request, 'core/product.html')
