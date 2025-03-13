7739d117226dc9535ff288578a47af8f93a65d60
BharatSingh <bharat2808singh@gmail.com>
2025-03-14 01:35:27 +0530

    copy phase 5

---
+++ 6_django/day14/lastphase4/bragbags_phase4/core/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import CustomerDetail, Luggage, Cart , Order
+# Register your models here.
+
+@admin.register(CustomerDetail)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display= ['id','user','name','address','city','state','pincode']
+
+
+@admin.register(Luggage)
+class PetAdmin(admin.ModelAdmin):
+    list_display= ['id','name','category','small_description','description','original_price','discounted_price', 'luggage_image']
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display= ['id','user','product','quantity']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display= ['id','user','customer','pet','quantity','order_at','status','total_price']
