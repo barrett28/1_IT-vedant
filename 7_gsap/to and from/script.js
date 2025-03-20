25584da4ef31f1f3a3e20684b14d4ec39037c243
BharatSingh <bharat2808singh@gmail.com>
2025-03-20 16:04:16 +0530

    complete phase9, order.html remaning

--- 7_gsap/to and from/script.js
+++ 7_gsap/to and from/script.js
@@ -8,3 +8,14 @@
 //   yoyo: true,
 //   scale: 0.75,
 // });
+
+// gsap.to("#box1", {
+//   x: 1200,
+//   duration: 2,
+//   delay: 1,
+//   rotate: 360,
+//   scale: 0.5,
+//   borderRadius: "50%",
+//   repeat: -1,
+//   yoyo: true,
+// });
