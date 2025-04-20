98cc7014e39f499f3173d30545852e956e4b4e19
BharatSingh <bharat2808singh@gmail.com>
2025-04-21 01:50:06 +0530

    most updated one phase 13

--- 4_sql/1_revise.sql
+++ 4_sql/1_revise.sql
@@ -9,6 +9,33 @@ create table employee(
   joining_date DATE
 );
 
+
+insert into employee(id, name, department, salary, joining_date, email)
+values(6, "Amit", "Sales", 410000, "2023-02-15", "amit@gmail.com");
+
+update employee
+set dept_id = 3
+where id = 6;
+
+update employee
+set dept_id = 1
+where department = "IT";
+
+update employee
+set dept_id = 2
+where department = "HR";
+
+update employee
+set dept_id = 3
+where department = "Sales";
+
+update employee
+set dept_id = 4
+where department = "Finance";
+
+select * from employee;
+
+
 insert into employee(id, name, department, salary, joining_date)
 values(1, "Rahul", "IT", 60000, "2020-01-15"),
 (2, "Priya", "HR", 50000, "2021-03-22"),
@@ -35,11 +62,32 @@ where id = 5;
 select DISTINCT department from employee;
 
 create table departments(
-	id INT,
+	dept_id INT,
     name VARCHAR(50)
 );
 
-alter table departments change column dept_id id INT (20);
+alter table departments
+add primary key (dept_id);
+
+alter table departments
+drop foreign key fk_employee;
+
+alter table departments
+drop column id;
+
+select * from departments;
+
+alter table employee
+add column dept_id INT,
+add constraint fk_deptid
+foreign key (dept_id) references departments(dept_id);
+
+select * from employee;
+
+
+
+
+alter table departments change column id dept_id INT (20);
 
 select * from departments;
 
@@ -54,5 +102,63 @@ inner join departments as d
 on e.department = d.name;
 
 
-select avg(salary) as average salary from employee 
+select department, avg(salary) as average_salary from employee 
 group by department;
+
+select department, avg(salary) as average_salary from employee
+group by department
+having avg(salary)>55000;
+
+select name, salary from employee 
+where salary > (select avg(salary) from employee); 
+
+select avg(salary) from employee;
+
+
+select name, salary from employee
+where department IN ("IT", "HR") 
+and salary between 50000 and 60000 
+and (name like "P%" or "R%");
+
+select name, salary, 
+	case 
+		when salary >= 60000 then 'High'
+        when salary between 50000 and 59999 then 'Medium'
+        when salary <= 50000 then 'Low'
+	end as salary_grade
+from employee;
+
+
+alter table employee 
+modify column email varchar(50) NOT NULL;
+
+update employee
+set email="hcbd@hbk.com" where name="Rahul";
+
+update employee 
+set email=null
+where name="Rahul";
+
+UPDATE employee SET email = 'rahul@example.com' WHERE name = 'Rahul';
+UPDATE employee SET email = 'priya@example.com' WHERE name = 'Priya';
+UPDATE employee SET email = 'amit@example.com' WHERE name = 'Amit';
+UPDATE employee SET email = 'shreya@example.com' WHERE name = 'Shreya';
+
+
+select * from employee;
+
+select d.name , AVG(e.salary) AS average_salary 
+from departments as d
+join employee as e on d.dept_id = e.dept_id
+group by e.department;
+
+SELECT d.name AS department, AVG(e.salary) AS average_salary
+FROM departments AS d
+JOIN employee AS e ON d.dept_id = e.dept_id
+GROUP BY d.name;
+
+
+
+
+
+
