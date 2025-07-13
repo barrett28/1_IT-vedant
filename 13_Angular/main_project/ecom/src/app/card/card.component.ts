78dd58e85904de56f7adc32d4c350b8940cf5888
Bharat <bharat2808singh@gmail.com>
2025-07-13 13:17:02 +0530

    cards ngClass

--- 13_Angular/main_project/ecom/src/app/card/card.component.ts
+++ 13_Angular/main_project/ecom/src/app/card/card.component.ts
@@ -6,13 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-  name: string = 'Mountains Ranges';
-  desc: string = 'Shayadri Mountain Ranges Explore the thrilling experience';
-  price: number = 15000;
-  img: string =
-    'https://images.unsplash.com/photo-1744649781353-8a1b70c37a77?q=80&w=1217&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+  // name: string = 'Mountains Ranges';
+  // desc: string = 'Shayadri Mountain Ranges Explore the thrilling experience';
+  // price: number = 15000;
+  // img: string =
+  //   'https://images.unsplash.com/photo-1744649781353-8a1b70c37a77?q=80&w=1217&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
   myName = '';
   isDisabled = false;
+
+  product1 = {
+    name: 'Lakes',
+    desc: 'Beautiful Uncrowded lakes',
+    price: 10000,
+    img: 'https://images.unsplash.com/photo-1720884413532-59289875c3e1?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  };
+
+  products: any = [
+    {
+      name: 'Mountains Ranges',
+      desc: 'Shayadri Mountain Ranges Explore the thrilling experience',
+      price: 15000,
+      img: 'https://images.unsplash.com/photo-1744649781353-8a1b70c37a77?q=80&w=1217&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      name: 'Lakes',
+      desc: 'Beautiful Uncrowded lakes',
+      price: 10000,
+      img: 'https://images.unsplash.com/photo-1720884413532-59289875c3e1?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+  ];
 
   onClickButton() {
     console.log('button clicked');
